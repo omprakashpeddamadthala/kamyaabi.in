becbcaec705f42db5ecb12138595c0f927128191
Om Prakash Peddamadthala <omprakashpeddamadthala@hotmail.com>
2026-05-29 03:36:15 +0000

    feat: add Google site verification file and meta tag

--- kamyaabi-frontend/src/components/common/SiteHead.tsx
+++ kamyaabi-frontend/src/components/common/SiteHead.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { config } from '../../config';
 
+const GOOGLE_SITE_VERIFICATION = '1e586fe8a6a37882';
+
 /**
  * Global, site-wide <head> tags rendered on every page:
+ *  - Google Search Console site-verification meta tag (backup to the static
+ *    /google1e586fe8a6a37882.html file).
  *  - Google Analytics (gtag.js) loaded as early as the app mounts.
  */
 const SiteHead: React.FC = () => {
@@ -11,6 +15,7 @@ const SiteHead: React.FC = () => {
 
   return (
     <Helmet>
+      <meta name="google-site-verification" content={GOOGLE_SITE_VERIFICATION} />
       {gaId && (
         <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} />
       )}
